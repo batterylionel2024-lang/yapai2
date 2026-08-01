f971330ddc0b4abc532d5ab6ae6d424469784652
石斌 <inselfteaching@gmail.com>
2026-08-01 16:02:10 +0800

    change details

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -2,7 +2,7 @@ class ContactMessage < ApplicationRecord
   after_commit :clear_dashboard_cache
 
   validates :name, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true
   validates :subject, presence: true
   validates :message, presence: true
 
