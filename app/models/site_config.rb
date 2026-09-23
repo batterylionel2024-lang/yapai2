83cc54078451ffb9edc54bafa24cbed946b70583
石斌 <inselfteaching@gmail.com>
2026-09-23 08:02:52 +0800

    2

--- app/models/site_config.rb
+++ app/models/site_config.rb
@@ -3,12 +3,6 @@ class SiteConfig < ApplicationRecord
   has_one_attached :favicon
   has_one_attached :sitemap
   has_one_attached :robots
-  
-  has_one_attached :infra_image_1
-  has_one_attached :infra_image_2
-  has_one_attached :infra_image_3
-  has_one_attached :infra_image_4
-  has_many_attached :factory_images
 
   def self.get
     first_or_create!(name: "Yapai")
